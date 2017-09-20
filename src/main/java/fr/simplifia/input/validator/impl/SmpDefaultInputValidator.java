cc007ecc20476ec59a1acbb86b8b4542da5e4c1f
ihebbenaziza <iheb.benaziza@ensi-uma.tn>
2017-09-20 02:44:57 +0100

    Add files via upload

--- src/main/java/fr/simplifia/input/validator/impl/SmpDefaultInputValidator.java
+++ src/main/java/fr/simplifia/input/validator/impl/SmpDefaultInputValidator.java
@@ -3,19 +3,26 @@ package fr.simplifia.input.validator.impl;
 import fr.simplifia.input.exception.InputException;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SmpDefaultInputValidator extends AbstractSmpInputValidator {
 
-    private static String REGEXP = ".*";
+    private  static Pattern enpattern = Pattern.compile (".[^éèê]*");
+
 
     protected SmpDefaultInputValidator(Locale locale) {
         super(locale);
     }
 
     @Override
-    public void validateInput(final String input) throws InputException {
-        if(!input.matches(REGEXP)){
+    public String validateInput(final String input) throws InputException {
+        Matcher matcher = enpattern.matcher (input);
+        if(!matcher.matches()&& locale.equals(Locale.ENGLISH)){
             throw new InputException("The input given is not compatible with the default validation strategy");
+
+        }else{
+            return input;
         }
     }
 }
