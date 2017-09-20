3eaa237b4b5ea8419764a3f1609d4c06a8426c8a
ihebbenaziza <iheb.benaziza@ensi-uma.tn>
2017-09-20 03:18:15 +0100

    Add files via upload

--- src/test/java/fr/simplifia/transform/SmpDataTransformerTest.java
+++ src/test/java/fr/simplifia/transform/SmpDataTransformerTest.java
@@ -1,9 +1,12 @@
 package fr.simplifia.transform;
 
 import fr.simplifia.input.validator.SmpInputValidator;
+import org.junit.Before;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.testng.annotations.Test;
 
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.*;
 
@@ -11,11 +14,17 @@ import static org.testng.Assert.*;
 public class SmpDataTransformerTest {
 
     private SmpInputValidator validator;
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     public SmpDataTransformerTest(){
         validator = Mockito.mock(SmpInputValidator.class);
         //TODO : mocking strategy
-        //when(validator.validateInput(input)).thenReturn();
+        String input=anyString();
+        when(validator.validateInput(input)).thenReturn(input);
+        assertEquals(validator.validateInput(input),input);
     }
 
     @Test
