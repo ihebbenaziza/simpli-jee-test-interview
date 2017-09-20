a176ce83af53bd87f4bc52a414d659ac91c217ca
ihebbenaziza <iheb.benaziza@ensi-uma.tn>
2017-09-20 03:17:43 +0100

    Add files via upload

--- src/test/java/fr/simplifia/output/impl/SmpOutputWriterImplTest.java
+++ src/test/java/fr/simplifia/output/impl/SmpOutputWriterImplTest.java
@@ -1,18 +1,27 @@
 package fr.simplifia.output.impl;
 
 import java.io.PrintStream;
+import java.util.Locale;
 
+import fr.simplifia.input.locale.LocaleExtractor;
+import org.junit.Before;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.testng.annotations.Test;
 
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by simplifia on 18/09/17.
  */
 public class SmpOutputWriterImplTest {
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     private PrintStream stream;
 
@@ -24,16 +33,23 @@ public class SmpOutputWriterImplTest {
     //TODO
     @Test
     public void testPrint() throws Exception {
+
+
+
     }
 
     //TODO
     @Test
     public void testPrintEmpty() throws Exception {
+        stream = Mockito.mock(PrintStream.class);
+        assertEquals(stream, "");
     }
 
     //TODO
     @Test
     public void testPrintNull() throws Exception {
+        stream = Mockito.mock(PrintStream.class);
+        assertEquals(stream, null);
     }
 
 }
